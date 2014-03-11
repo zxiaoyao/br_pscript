05015df04e01b7f38ab3f44de4a6e7d727de8a02
zxiaoyao <zhuxuyu@gmail.com>
2014-03-10 22:21:26 -0400

    add another script

---
+++ pymol/parse_net.py
@@ -0,0 +1,8 @@
+'''
+Created on Feb 19, 2014
+
+@author: xzhu
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ sys_admin/repairSoftLink.py
@@ -0,0 +1,28 @@
+'''
+Created on Mar 10, 2014
+
+@author: xzhu
+'''
+
+import os, time, sys
+
+def abSoftLink(relativeLink):
+    ''' Change the relative soft link to an absolute soft link.
+    '''
+    
+    pointPath = relativeLink
+    while os.path.islink(pointPath):
+        pointPath = os.readlink(pointPath)
+        
+    return pointPath
+
+if __name__ == '__main__':
+#     for eachFile in os.listdir("/Users/xzhu/sibyl"):
+#         abPath = os.path.join("/Users/xzhu/sibyl", eachFile)
+#         print abPath
+#         print os.stat(abPath).st_nlink, time.ctime(os.stat(abPath).st_ctime), os.stat(abPath).st_dev
+#     for dirName, subDirs, subFiles in os.walk("/Users/xzhu/sibyl/BR2/1C3W/hydro/def"):
+#         for eachFile in subFiles:
+#             if os.path.islink(os.path.join(dirName, eachFile)):
+#                 print os.path.join(dirName, eachFile)
+    print abSoftLink(sys.argv[1])
