7e0fd7fb70806ec00776df7258347930d8d6ccce
zxiaoyao <zhuxuyu@gmail.com>
2014-05-27 18:57:39 -0400

    foo is used to basic testing.

---
+++ foo/t1.py
@@ -0,0 +1,23 @@
+'''
+Created on May 27, 2014
+
+@author: xzhu
+
+I'm really very happy that I found it.
+Just one number in the ip address of localhost.
+'''
+import networkx as nx
+
+def fun():
+    print "Just for fun"
+    
+
+class f:
+    def __init__(self):
+        self.a = 100
+        
+        
+if __name__ == '__main__':
+    g = nx.Graph()
+    print "hehe"
+    print f().a
